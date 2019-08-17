fc0be04e068abbb08a1f7f6674b7c7935617ce84
zczcode <15632604095@163.com>
2019-08-17 11:41:57 +0800

    备份

---
+++ src/main/java/com/mmall/common/Const.java
@@ -0,0 +1,17 @@
+package com.mmall.common;
+
+/**
+ * Created by Zcz
+ */
+public class Const {
+
+    public static final String CURRENT_USER = "currentUser";
+
+    public static final String EMAIL = "email";
+    public static final String USERNAME = "username";
+
+    public interface Role{
+        int ROLE_CUSTOMER = 0;//普通用户
+        int ROLE_ADMINN = 1;//管理员
+    }
+}
---
+++ src/main/java/com/mmall/service/IUserService.java
@@ -0,0 +1,22 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+
+/**
+ * Created by Zcz
+ */
+public interface IUserService {
+
+    ServerResponse<User> login(String username, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str,String type);
+
+    ServerResponse selectQuestion(String username);
+
+    ServerResponse<String> checkAnswer(String username,String question,String answer);
+
+    ServerResponse<String> forgetResetPassword(String username,String passwordNew,String forgetToken);
+}
